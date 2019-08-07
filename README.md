65c28a3f8f158abaf1cd758b7c5ea9ed63678ef8
Brent Shepherd <thenbrent@gmail.com>
2019-08-07 16:29:28 +1000

    Initial commit of AS Test Data Generator

--- README.md
+++ README.md
@@ -1,12 +1,37 @@
-# {plugin_name}
+# Action Scheduler - Test Data Generator
 
-{plugin_short_description}
+Generate a large number of dummy actions with Action Scheduler.
+
+### Usage
+
+Append `?astdg_generate_actions=true` to an admin page to generate actions.
+
+Additional parameters available:
+
+* `(string)'astdg_action_type'`: Type of action to create. Can be `async`, `single`, `cron` or `recurring`. Default `async`.
+* `(int)'astdg_start_time'`: Unix timestamp representing time to run the action, or in the case of Cron actions, time to schedule the first instance at or after. Not used for `async` actions. Default current time.
+* `(string)'astdg_hook'`: String to use as the action's hook. Default 'astdg_test_{$action_type}_action'.
+* `(string)'astdg_group'`: Group for the actions. Default 'astdg_test_{$action_type}_actions'.
+* `(int)'astdg_sleep_time'`: Optional amount of time to sleep when the action runs. Only works if using default hook value. Default `0`.
+* `(int)'astdg_action_count'`: Number of actions to create. Default 100. 
+* `(int)|(string)'astdg_recurrence'`: Recurrence for recurring and cron actions.
+	* For a recurring action, this should be an `int representing number of seconds between each recurrence. Default, every 12 hours: `43,200`.
+	* For a cron action, this should be an `string` representing the cron schedule. Default, every 12 hours: `'0 */12 * * *'`.
+
+#### Examples URLs:
+
+* Create 10 Cron actions that run every 12 hours: `/wp-admin/?astdg_generate_actions=true&astdg_action_type=cron&astdg_action_count=10`
+* Create 10 Cron actions that run once every day: `/wp-admin/?astdg_generate_actions=true&astdg_action_type=cron&astdg_action_count=10&0 0 * * *`
+* Create 250 Recurring actions to recur every 12 hours: `/wp-admin/?astdg_generate_actions=true&astdg_action_type=recurring&astdg_action_count=250`
+* Create 250 Recurring actions to recur every 24 hours: `/wp-admin/?astdg_generate_actions=true&astdg_action_type=recurring&astdg_action_count=250&recurrence=86400`
+* Create 100 Single actions with hook `'my_test_action'`: `/wp-admin/?astdg_generate_actions=true&astdg_action_type=single&astdg_hook=my_test_action`
+* Create 1,000 Single actions to run 1st January 2020: `/wp-admin/?astdg_generate_actions=true&astdg_action_type=single&astdg_action_count=1000&astdg_start_time=1577836800`
 
 ## Installation
 
 To install:
 
-1. Download the latest version of the plugin [here](https://github.com/Prospress/{plugin_slug}/archive/master.zip)
+1. Download the latest version of the plugin [here](https://github.com/Prospress/action-scheduler-test-data-generator/archive/master.zip)
 1. Go to **Plugins > Add New > Upload** administration screen on your WordPress site
 1. Select the ZIP file you just downloaded
 1. Click **Install Now**
@@ -18,7 +43,7 @@ To keep the plugin up-to-date, use the [GitHub Updater](https://github.com/afrag
 
 ## Reporting Issues
 
-If you find an problem or would like to request this plugin be extended, please [open a new Issue](https://github.com/Prospress/{plugin_slug}/issues/new).
+If you find an problem or would like to request this plugin be extended, please [open a new Issue](https://github.com/Prospress/action-scheduler-test-data-generator/issues/new).
 
 ---
 
---
+++ action-scheduler-test-data-generator.php
@@ -0,0 +1,89 @@
+<?php
+/*
+ * Plugin Name: Action Scheduler - Test Data Generator
+ * Plugin URI: https://github.com/Prospress/action-scheduler-test-data-generator/
+ * Description: Generate a large number of dummy actions with Action Scheduler.
+ * Author: Prospress Inc.
+ * Author URI: https://prospress.com/
+ * License: GPLv3
+ * Version: 1.0.0
+ * Requires at least: 4.0
+ * Tested up to: 4.8
+ *
+ * GitHub Plugin URI: Prospress/action-scheduler-test-data-generator
+ * GitHub Branch: master
+ *
+ * Copyright 2018 Prospress, Inc.  (email : freedoms@prospress.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package		Action Scheduler - Test Data Generator
+ * @author		Prospress Inc.
+ * @since		1.0
+ */
+
+function as_test_schedule_async_action() {
+
+	if ( ! is_admin() || ! isset( $_GET['astdg_generate_actions'] ) ) {
+		return;
+	}
+
+	$action_type  = isset( $_GET['astdg_action_type'] ) ? $_GET['astdg_action_type'] : 'async';
+	$start_time   = isset( $_GET['astdg_start_time'] ) ? $_GET['astdg_start_time'] : time();
+	$hook         = isset( $_GET['astdg_hook'] ) ? $_GET['astdg_hook'] : sprintf( 'astdg_test_%s_action', $action_type );
+	$group        = isset( $_GET['astdg_group'] ) ? $_GET['astdg_group'] : sprintf( 'astdg_test_%s_actions', $action_type );
+	$sleep_time   = isset( $_GET['astdg_sleep_time'] ) ? (int) $_GET['astdg_sleep_time'] : 0;
+	$action_count = isset( $_GET['astdg_action_count'] ) ? (int) $_GET['astdg_action_count'] : 100;
+
+	$args = array(
+		'creation_timestamp' => time(),
+		'sleep_time' => 1,
+	);
+
+	for ( $i = 1; $i <= $action_count; $i++ ) {
+
+		$args['i'] = $i;
+
+		switch ( $action_type ) {
+			case 'async' :
+				as_enqueue_async_action( $hook, $args, $group );
+				break;
+			case 'cron' :
+				$recurrence = isset( $_GET['astdg_recurrence'] ) ? $_GET['astdg_recurrence'] : '0 */12 * * *';
+				as_schedule_cron_action( $start_time, $recurrence, $hook, $args, $group );
+				break;
+			case 'recurring' :
+				$recurrence = isset( $_GET['astdg_recurrence'] ) ? (int) $_GET['astdg_recurrence'] : 12 * HOUR_IN_SECONDS;
+				as_schedule_recurring_action( $start_time, $recurrence, $hook, $args, $group );
+				break;
+			case 'single' :
+				as_schedule_single_action( $start_time, $hook, $args, $group );
+				break;
+		}
+	}
+}
+add_action( 'shutdown', 'as_test_schedule_async_action' );
+
+/**
+ * Add a default sleep based on params.
+ */
+function as_test_action_sleep( $creation_timestamp, $sleep_time ) {
+	if ( $sleep_time > 0 ) {
+		sleep( $sleep_time );
+	}
+}
+add_action( 'astdg_test_async_action', 'as_test_action_sleep', 10, 2 );
+add_action( 'astdg_test_cron_action', 'as_test_action_sleep', 10, 2 );
+add_action( 'astdg_test_single_action', 'as_test_action_sleep', 10, 2 );
+add_action( 'astdg_test_recurring_action', 'as_test_action_sleep', 10, 2 );
